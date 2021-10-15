eebb68374d0752dd1c3f248476616dfd17b3abf5
pepijn <pepijn@pdik.nl>
2021-10-15 19:59:21 +0200

    Changed Faq Nav body and footer

--- wwwroot/assets/js/app.js
+++ wwwroot/assets/js/app.js
@@ -1,26 +1,34 @@
 import FYSCloud from "https://cdn.fys.cloud/fyscloud/0.0.4/fyscloud.es6.min.js";
+import "./config.js";
+document.addEventListener("DOMContentLoaded", async function () {
+    //Add the header to the page
+    const headerData = await FYSCloud.Utils.fetchAndParseHtml("_header.html");
+    addHeader(headerData);
 
-/**
- * Load Header
- */
-function loadHeader(){
-    FYSCloud.Utils.fetchAndParseHtml("_header.html")
-        .then(function (data) {
-            const firstElement = data[0];
-            document.body.insertBefore(firstElement, document.body.firstChild);
-        });
-}
+    //Add the footer to the page
+    const footerData = await FYSCloud.Utils.fetchAndParseHtml("_footer.html");
+    addFooter(footerData);
 
-/**
- * LoadFooter
- */
-function loadFooter(){
-    FYSCloud.Utils.fetchAndParseHtml("_footer.html")
-        .then(function (data) {
-            const firstElement = data[0];
-            document.body.insertBefore(firstElement, document.body.lastChild);
-        });
-}
+    function addHeader(data) {
+        const firstElement = data[0];
+        // checkLoggedIn(firstElement);
+        document.querySelector("#nav").appendChild(firstElement);
+        // document.body.insertBefore(firstElement, document.body.firstChild);
+    }
 
-loadHeader();
-loadFooter();
+    function addFooter(data) {
+        const firstElement = data[0];
+        document.querySelector("#footer").appendChild(firstElement);
+    }
+});
+
+//NOTE: Global function so other JavaScript files can use this as well
+// export function checkLoggedIn(element) {
+//     if (FYSCloud.Session.get("loggedin")) {
+//         element.querySelectorAll(".js-loggedin-show").forEach(e => e.style.display = "block");
+//         element.querySelectorAll(".js-loggedin-hide").forEach(e => e.style.display = "none");
+//     } else {
+//         element.querySelectorAll(".js-loggedin-show").forEach(e => e.style.display = "none");
+//         element.querySelectorAll(".js-loggedin-hide").forEach(e => e.style.display = "block");
+//     }
+// }
