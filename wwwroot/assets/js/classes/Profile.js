95064b0e487ff9631e190bf355dc269e0a646287
pepijn <pepijn@pdik.nl>
2021-12-02 10:51:00 +0100

    Added nav to profile page

--- wwwroot/assets/js/classes/Profile.js
+++ wwwroot/assets/js/classes/Profile.js
@@ -6,20 +6,42 @@ import "../config.js";
 
 export default class Profile {
 
+    // public first_name;
+    // public last_name;
+    // private password;
+    // private email;
+    // private account_type;
+    // private birthday;
+    // private qountry;
+    // private id;
+    // private gender;
 
     constructor() {
         this.id = FYSCloud.Session.get('user_id');
     }
 
+    /**
+     * Setter
+     * @param id
+     */
+    setId(id){
+        this.id = id;
+        FYSCloud.Session.set('user_id', this.id);
+    }
     getFullName() {
         return this.first_name + " " + this.last_name;
     }
 
+    /**
+     * Register a new Profile and set this profile data
+     * @return {Promise<{}|*>}
+     */
     async registerProfile() {
         if (this.id === 'undefined') {
             try {
                 let data = await FYSCloud.API.queryDatabase("INSERT INTO users (first_name, last_name, password,email,account_type,birthday,qountry_origin_id) VALUES (?,?,?,?,?,?,?);", [this.first_name, this.last_name, this.password, this.email, this.account_type, this.birthday, this.qountry]);
-                return data[0]
+                this.setId(data[0].id) //Set registerd user
+                await this.setProfile(); //Set all profile data
             } catch (e) {
                 return {};
             }
@@ -46,6 +68,7 @@ export default class Profile {
         this.account_type = data.account_type;
         this.birthday = data.birthday;
         this.profile = data.profile;
+        this.gender = data.gender;
     }
 
     /**
