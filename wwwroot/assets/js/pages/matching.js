5135687a46497d779fbe3bc4cd31c1ca59852942
Jonah Brandwagt <contact@jonahbrandwagt.com>
2021-12-07 20:31:57 +0100

    Connected matching to database, Added dynamic profile
    Profile example url's: /profiel.html | /profiel.html?id=1

--- wwwroot/assets/js/pages/matching.js
+++ wwwroot/assets/js/pages/matching.js
@@ -1,6 +1,8 @@
 import {GetCurrentPage} from "../app.js";
 import Profile from "../classes/Profile.js";
 import Countries from "../Objects/Countries.js";
+import FYSCloud from "https://cdn.fys.cloud/fyscloud/0.0.4/fyscloud.es6.min.js";
+import "../config.js";
 const profiel = new Profile();
 await profiel.setProfile();
 
@@ -16,3 +18,43 @@ profile_age.innerHTML = profiel.birthday;
 
 var profile_orgin =document.querySelector('#mathing_profile_orgin');
 profile_orgin.innerHTML = profiel.getQountry();
+
+let users = await FYSCloud.API.queryDatabase('SELECT * FROM users')
+let countries = await FYSCloud.API.queryDatabase('SELECT * FROM countries')
+
+for(var user in users) {
+    user = users[user]
+
+    //getting age from current date / users birthday
+    const age = new Date().getFullYear() - new Date(user.birthday).getFullYear()
+
+    //filtering to get users country (name)
+    const country = countries.filter(country => country.id == user.country_origin_id)[0]
+
+    //capitalizing first letter of country name
+    const arr = country.names.split(" ");
+    for (var i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+
+    }
+    const formatted_country_name = arr.join(" ");
+
+    //capitalizing first letter of gender
+    const formatted_gender = user.gender.charAt(0).toUpperCase() + user.gender.slice(1)
+    
+    //adding user card to container
+    document.getElementById('card-container').innerHTML += `
+    <div class="grid-child">
+        <div class="card" onclick="window.open('profiel.html?id=${user.id}');" style="cursor: pointer;">
+            <div id="image">
+                <img class="align_image" src="${user.profile}" alt="Profile Picture">
+            </div>
+            <p id="user_name">${user.first_name} ${user.last_name}</p>
+            <div id="info">
+                <p>${age}</p>
+                <p>${formatted_country_name}</p>
+                <p>${formatted_gender}</p>
+            </div>
+        </div>
+    </div>`
+}
--- wwwroot/assets/js/pages/profiel.js
+++ wwwroot/assets/js/pages/profiel.js
@@ -1,31 +1,94 @@
+import FYSCloud from "https://cdn.fys.cloud/fyscloud/0.0.4/fyscloud.es6.min.js";
 import {GetCurrentPage, redirect} from "../app.js";
 import Profile from "../classes/Profile.js";
 import Countries from "../Objects/Countries.js";
 
-const profiel = new Profile();
-await profiel.setProfile();
+let query = FYSCloud.URL.queryString()
 
+if(query.id > 0) {
+    var user = await FYSCloud.API.queryDatabase('SELECT * FROM users WHERE id = ?', [query.id])
 
-var fullName = document.querySelector('.profile_name');
-fullName.innerHTML = profiel.getFullName();
+    let countries = await FYSCloud.API.queryDatabase('SELECT * FROM countries')
 
-var bio = document.querySelector('.profile_bio');
-var intress = document.querySelector('.profile_name');
+    console.log(user)
+    console.log(countries)
 
-var age = document.querySelector('#profiel_age');
-const d = new Date(profiel.birthday);
+    if(user.length > 0) { 
+        user = user[0]
+        var fullName = document.querySelector('.profile_name');
+        fullName.innerHTML = user.first_name + ' ' + user.last_name;
 
-age.innerHTML = d.toLocaleDateString().replace("T", " ");
+        var bio = document.querySelector('.profile_bio');
+        var intress = document.querySelector('.profile_name');
 
-var gender = document.querySelector('#profiel_gender');
-var country = document.querySelector('#profiel_country');
-country.innerHTML = profiel.getQountry();
+        var age = document.querySelector('#profiel_age');
+        const d = new Date(user.birthday);
 
-document.getElementById("avatar").src = profiel.getProfilePicture();
+        age.innerHTML = d.toLocaleDateString().replace("T", " ");
 
-const change_profile = document.querySelector(".btn_edit");
-console.log(change_profile);
-change_profile.addEventListener('click', edit);
+        //filtering to get users country (name)
+        const country = countries.filter(country => country.id == user.country_origin_id)[0]
+
+        //capitalizing first letter of country name
+        const arr = country.names.split(" ");
+        for (var i = 0; i < arr.length; i++) {
+            arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+
+        }
+        const formatted_country_name = arr.join(" ");
+
+        //capitalizing first letter of gender
+        const formatted_gender = user.gender.charAt(0).toUpperCase() + user.gender.slice(1)
+
+        var genderObject = document.querySelector('#profiel_gender');
+        genderObject.innerHTML = formatted_gender
+        var countryObject = document.querySelector('#profiel_country');
+        countryObject.innerHTML = formatted_country_name
+
+        document.getElementById("avatar").src = user.profile
+    } else {
+        redirect('404.htm')
+    }
+} else {
+    const profiel = new Profile();
+    await profiel.setProfile();
+
+    var fullName = document.querySelector('.profile_name');
+    fullName.innerHTML = profiel.getFullName();
+
+    var bio = document.querySelector('.profile_bio');
+    var intress = document.querySelector('.profile_name');
+
+    var age = document.querySelector('#profiel_age');
+    const d = new Date(profiel.birthday);
+
+    age.innerHTML = d.toLocaleDateString().replace("T", " ");
+
+    var gender = document.querySelector('#profiel_gender');
+
+    //capitalizing first letter of gender
+    const formatted_gender = profiel.gender.charAt(0).toUpperCase() + profiel.gender.slice(1)
+
+    gender.innerHTML = formatted_gender
+
+    var country = document.querySelector('#profiel_country');
+
+    //capitalizing first letter of country name
+    const arr = profiel.getQountry().split(" ");
+    for (var i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+
+    }
+    const formatted_country_name = arr.join(" ");
+
+    country.innerHTML = formatted_country_name
+
+    document.getElementById("avatar").src = profiel.getProfilePicture();
+
+    const change_profile = document.querySelector(".btn_edit");
+    console.log(change_profile);
+    change_profile.addEventListener('click', edit);
+}
 
 function edit() {
     redirect('profiel_edit.html')
