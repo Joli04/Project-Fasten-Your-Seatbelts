99089ebf6f4d83a8a696a74d96ddba6ffc2957bc
pepijn <pepijn@pdik.nl>
2021-12-09 12:46:43 +0100

    Added Mail image header

--- wwwroot/assets/js/classes/Profile.js
+++ wwwroot/assets/js/classes/Profile.js
@@ -269,7 +269,7 @@ export default class Profile {
                         "                <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\" style=\"max-width: 600px;\">\n" +
                         "                    <tr>\n" +
                         "                        <td bgcolor=\"#ffffff\" align=\"center\" valign=\"top\" style=\"padding: 40px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #111111; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;\">\n" +
-                        "                            <h1 style=\"font-size: 48px; font-weight: 400; margin: 2;\">Welkom!</h1> <img src=\"https://dev-is108-3.fys.cloud/assets/img/hero.jpg\" width=\"125\" height=\"120\" style=\"display: block; border: 0px;\" />\n" +
+                        "                            <h1 style=\"font-size: 48px; font-weight: 400; margin: 2;\">Welkom!</h1> <img src=\"https://dev-is108-3.fys.cloud/assets/img/corendon-logo.svg\" width=\"125\" height=\"120\" style=\"display: block; border: 0px;\" />\n" +
                         "                        </td>\n" +
                         "                    </tr>\n" +
                         "                </table>\n" +
