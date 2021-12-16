a2b52be73d68a96990e4bb61a9827a56d88c89af
pepijn <pepijn@pdik.nl>
2021-12-16 13:14:59 +0100

    Register

--- wwwroot/assets/js/Controllers/Register_Controller.js
+++ wwwroot/assets/js/Controllers/Register_Controller.js
@@ -2,14 +2,62 @@
  * Profile controller
  */
 import Controller from './Controller.js';
-
+import App from '../Classes/app.js';
+import Profile from "../Classes/Profile.js";
+import Countries from "../Objects/Countries.js";
+const profiel = new Profile();
 import view from "../Classes/View.js";
 
-export default class Home_Controller extends Controller
+
+export default class Register_Controller extends Controller
 {
     show() {
+        /**
+         * Added Script to header
+         * @type {HTMLScriptElement}
+         */
+        const script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = "./vendors/jsSHA/sha.js";
+        document.head.appendChild(script);
+        window.onchange = function () {
+            const selector = document.querySelector("#country_selector");
+            selector.innerHTML= "";
+            Countries.initCountrieSelector(selector);
+            const registerBtn = document.querySelector("#aanmelden");
+            if(registerBtn) {
+
+                registerBtn.addEventListener('click', Register_Controller.register);
+            }
+        };
 
     }
+    static register(){
+    let elements = document.querySelectorAll("form input");
+    if(App.validate(elements)){
+
+        const firstname =  document.querySelector('input[name="firstname"]');
+        const lastname =  document.querySelector('input[name="lastname"]');
+        const email =  document.querySelector('input[name="email"]');
+        const password =  document.querySelector('input[name="password"]');
+        const shaObj = new jsSHA("SHA-512", "TEXT", {encoding: "UTF8"});
+
+        shaObj.update(password.value);
+        const hash = shaObj.getHash("HEX");
+
+        const birthday = document.querySelector('input[name="birthday"]');
+        const genderValue = document.querySelector('#gender');
+        const gender = genderValue.options[genderValue.selectedIndex].value;
+        const country = document.getElementById("countries")
+        try{
+            return profiel.registerProfile(firstname.value,lastname.value,email.value,hash,birthday.value,gender,country.value);
+            //redirect("profiel.html");
+        }catch (e){
+            console.log(e);
+        }
+
+    }
+}
     render() {
         return new view('registratie.html',"Commonflight Home");
     }
