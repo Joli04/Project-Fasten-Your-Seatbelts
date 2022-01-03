4ec8cb0826617d98062a56dc459c4e4d29e084e7
diaq <diaqfort@gmail.com>
2022-01-03 18:57:11 +0100

    admin logout button

--- wwwroot/assets/js/Controllers/AdminBaseController.js
+++ wwwroot/assets/js/Controllers/AdminBaseController.js
@@ -6,6 +6,8 @@ import Controller from './Controller.js';
 import view from "../Classes/View.js";
 import Profile from "../Classes/Profile.js";
 import App from "../Classes/App.js";
+import Login_Controller from "./Login_Controller.js";
+
 export default class AdminBaseController extends Controller {
 
     constructor(model, view) {
@@ -15,10 +17,7 @@ export default class AdminBaseController extends Controller {
     {
         const profiel = new Profile();
         await profiel.setProfile();
-        if(profiel.account_type === "user"){
-            App.redirect("#/profiel")
-            App.ShowNotifyError("Geen toegang", "Je hebt helaas geen toegang tot deze pagina")
-        }else{
+        if(profiel.account_type === "admin"){
             //Remove Basic layouts
             document.querySelector("body").remove();
             document.querySelector("html").append(document.createElement("body"))
@@ -30,10 +29,19 @@ export default class AdminBaseController extends Controller {
             http.onreadystatechange = async function () {
                 if (this.readyState === 4 && this.status === 200) {
                     document.querySelector("body").innerHTML = this.response;
+
+                    const logoutBtn = document.querySelector('#adminNav_logout');
+                    logoutBtn.addEventListener('click', function() {
+                        Login_Controller.logout();
+                        window.location.reload();
+                    });
                 }
             };
             http.open('GET', url, true);
             http.send();
+        }else{
+            App.redirect("#/home")
+            App.ShowNotifyError("Geen toegang", "Je hebt helaas geen toegang tot deze pagina")
         }
 
     }
