0e0f80fdf0143e9cfc309104a7c0cc9856ab7183
pepijn <pepijn@pdik.nl>
2022-01-03 18:24:47 +0100

    Chat function exist fix

--- wwwroot/assets/js/Controllers/Chat_controller.js
+++ wwwroot/assets/js/Controllers/Chat_controller.js
@@ -12,6 +12,7 @@ import Messages from "../Objects/Messages.js";
 export default class Chat_controller extends Controller {
 
     async chat() {
+
         const script = document.createElement("script");
         script.type = "text/javascript";
         script.src = "./vendors/EmojiPicker/emojiPicker.js";
@@ -104,15 +105,18 @@ export default class Chat_controller extends Controller {
             sendBtn = form.querySelector(".send"),
             chatBox = document.querySelector(".chat-box");
 
-        new FgEmojiPicker({
-            trigger: ['.emojiBtn'],
-            position: ['bottom', 'right'],
-            dir:'vendors/EmojiPicker/',
-            emit(obj, EmojiBtn) {
-                const emoji = obj.emoji;
-                inputField.value += emoji;
-            }
-        });
+        if(!window.FgEmojiPicker){
+            new FgEmojiPicker({
+                trigger: ['.emojiBtn'],
+                position: ['bottom', 'right'],
+                dir:'vendors/EmojiPicker/',
+                emit(obj, EmojiBtn) {
+                    const emoji = obj.emoji;
+                    inputField.value += emoji;
+                }
+            });
+        }
+
         form.onsubmit = (e) => {
             e.preventDefault();
             scrollToBottom()
@@ -122,7 +126,9 @@ export default class Chat_controller extends Controller {
 
 
         sendBtn.onclick = async () => {
-            await this.messages.send(this.chat_id, inputField.value, this.other_id)
+            var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
+
+            await this.messages.send(this.chat_id, Base64.encode(inputField.value), this.other_id)
             if (true) {
                 inputField.value = "";
                 scrollToBottom();
@@ -151,6 +157,7 @@ export default class Chat_controller extends Controller {
                 time_text = "Today";
             }
             time.innerHTML= time_text +' '+Latest_chat.getUTCHours() + ':' + Latest_chat.getUTCMinutes();
+            var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
 
             for (let i = 0; i < messages.length; i++) {
                 let message = messages[i]
