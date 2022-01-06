e50f8a4b0dc35f953eef3c1ecc8d866ad3454629
diaq <diaqfort@gmail.com>
2022-01-06 01:59:32 +0100

    Changed chat styling for mobile

--- wwwroot/assets/js/Controllers/Chat_controller.js
+++ wwwroot/assets/js/Controllers/Chat_controller.js
@@ -60,11 +60,11 @@ export default class Chat_controller extends Controller {
 
         
         let allChats = await this.messages.getAllChats(this.profiel.id)
-        let menu_element = document.querySelector('.contacts')
+        let menu_element = document.querySelector('.contacts__users')
         const otherUserProfilePic = document.querySelector('#profile_pic');
         const otherUserProfileName = document.querySelector('.bar .name');
 
-        otherUserProfilePic.style.backgroundImage = this.other_profile.profile
+        // otherUserProfilePic.style.backgroundImage = this.other_profile.profile
         otherUserProfileName.innerHTML = this.other_profile.first_name + " " + this.other_profile.last_name
 
         for (let i = 0; i < allChats.length; i++) {
@@ -78,22 +78,32 @@ export default class Chat_controller extends Controller {
 
             if (currentChat.id == this.chat_id) {
                 const messages = await this.messages.get(this.chat_id);
-                menu_element.innerHTML += `<div class="contact"><div class="pic" style="background-image: url(${otherUserId.getProfilePicture()});"` +
-                    `></div><div class="badge">${messages.length}</div><div class="name">${otherUserId.getFullName()}</div>\n` +
-                    `                <div class="message">` +
-                    `                    Last chat` +
-                    `                </div>` +
-                    `            </a></div>`;
+                menu_element.innerHTML += `<div class="contact activeChat">
+                    <img class="pic" src="${otherUserId.getProfilePicture()}" alt="profile picture">
+                    <div class="contact__info">
+                    <div class="badge">${messages.length}</div>
+                    <div class="name">${otherUserId.getFullName()}</div>\n` +
+                    `<div class="message">` +
+                    `    Last chat` +
+                    `</div> </div>` +
+                    `</a></div>`;
+
+                otherUserProfilePic.innerHTML +=
+                    `<img class="contactInfo__image" src="${otherUserId.getProfilePicture()}" alt="other user profile picture">`;
                 //menu_element.innerHTML += `<a href="#/chat?id=${currentChat.id}" class="active">${otherUserId.first_name} ${otherUserId.last_name}</a>`
             } else {
                 //Show non active user
                 const messages = await this.messages.get(currentChat.id);
-                menu_element.innerHTML += `<a href="#/chat?id=${currentChat.id}"><div class="contact"><div class="pic" style="background-image: url(${otherUserId.getProfilePicture()});"` +
-                    `></div><div class="badge">${messages.length}</div><div class="name">${otherUserId.getFullName()}</div>\n` +
-                    `                <div class="message">` +
-                    `                    Last chat` +
-                    `                </div>` +
-                    `            </div></a>`;
+                menu_element.innerHTML += `<a href="#/chat?id=${currentChat.id}">
+                    <div class="contact">
+                    <img class="pic" src="${otherUserId.getProfilePicture()}" alt="profile picture">
+                    <div class="contact__info">
+                    <div class="badge">${messages.length}</div>
+                    <div class="name">${otherUserId.getFullName()}</div>\n` +
+                    `<div class="message">` +
+                    `    Last chat` +
+                    `</div></div>` +
+                    `</a>`;
                 // menu_element.innerHTML += `<a href="#/chat?id=${currentChat.id}">${otherUserId.first_name} ${otherUserId.last_name}</a>`
             }
 
@@ -165,7 +175,7 @@ export default class Chat_controller extends Controller {
             const Latest_chat = new Date(Math.max(...messages.map(e => new Date(e.message_send_at))));
             var time_text = "";
             if (isToday(Latest_chat)) {
-                time_text = "Today";
+                time_text = "Today at";
             }
             time.innerHTML = time_text + ' ' + Latest_chat.getUTCHours() + ':' + Latest_chat.getUTCMinutes();
             var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=0,c1=0,c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);var c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
@@ -179,16 +189,26 @@ export default class Chat_controller extends Controller {
 
                     message.msg = Base64.decode(message.msg);
 
-                    if (message.from_user_id == self.profiel.id) {
-                        chat_element.innerHTML += `<div class="message parker">
-                            ${message.msg}
-                        </div>`
-                    } else {
+                    const messageTime = new Date(message.message_send_at);
+                    let messageTimeHours = messageTime.getHours() - 1;
+                    let messageTimeMinutes = messageTime.getMinutes();
 
-                        chat_element.innerHTML += `
-                    <div class="message stark">
-                        ${message.msg}
-                    </div>`
+                    if (messageTimeHours < 10) {
+                        messageTimeHours = "0" + messageTimeHours;
+                    } else if (messageTimeMinutes < 10) {
+                        messageTimeMinutes = "0" + messageTimeMinutes;
+                    }
+
+                    if (message.from_user_id == self.profiel.id) {
+                        chat_element.innerHTML += `<div class="message parker">` +
+                            `<div class="message__content">${message.msg}</div>` +
+                            `<div class="message__time">${messageTimeHours + ':' + messageTimeMinutes}</div>` +
+                        `</div>`
+                    } else {
+                        chat_element.innerHTML += `<div class="message stark">` +
+                            `<div class="message__content">${message.msg}</div>` +
+                            `<div class="message__time">${messageTimeHours + ':' + messageTimeMinutes}</div>` +
+                            `</div>`
                     }
                     scrollToBottom()
                 }
