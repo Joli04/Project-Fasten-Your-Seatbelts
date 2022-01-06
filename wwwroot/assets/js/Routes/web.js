b6946f07823c1a1b6b63fdbd1762b48d782335de
diaq <diaqfort@gmail.com>
2022-01-06 16:05:22 +0100

    Changed stats to dashboard

--- wwwroot/assets/js/Routes/web.js
+++ wwwroot/assets/js/Routes/web.js
@@ -12,7 +12,6 @@ import Matches_Controller from "../Controllers/Matches_Controller.js";
 
 import Admin_Controller from '../Controllers/Admin_Controller.js';
 import AdminUsers_Controller from '../Controllers/AdminUsers_Controller.js';
-import AdminStats_Controller from '../Controllers/AdminStats_Controller.js';
 
 import MatchRequestController from "../Controllers/MatchRequestController.js";
 import Chat_controller from "../Controllers/Chat_controller.js";
@@ -77,10 +76,6 @@ const routes = [
     },true),
     new Route('admin/users', {
         controller: new AdminUsers_Controller,
-        method: 'index'
-    },true),
-    new Route('admin/stats', {
-        controller: new AdminStats_Controller,
         method: 'index'
     },true),
 ];
