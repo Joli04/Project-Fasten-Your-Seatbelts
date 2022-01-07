6f128ccddf9384e0be0571534f71085936e6535e
pepijn <pepijn@pdik.nl>
2022-01-07 14:00:06 +0100

    Admin user page
    Added Profile class to handle more user functions

--- wwwroot/assets/js/Controllers/AdminUsers_Controller.js
+++ wwwroot/assets/js/Controllers/AdminUsers_Controller.js
@@ -8,6 +8,7 @@ import view from "../Classes/View.js";
 import App from '../Classes/app.js';
 import AdminBaseController from "./AdminBaseController.js";
 import Admin from "../Classes/Admin.js";
+import Profile from "../Classes/Profile.js";
 
 export default class AdminUsers_Controller extends AdminBaseController
 {
@@ -33,7 +34,8 @@ export default class AdminUsers_Controller extends AdminBaseController
             const url = FYSCloud.Utils.createUrl("#/profiel", {
                 id: admin.users[i].id,
             });
-
+            const user = new Profile();
+            await user.setProfile(admin.users[i]);
             userList.innerHTML += `
                 <tr class="userList__user">
                     <th class="user__field user__name">${admin.users[i].first_name + " " + admin.users[i].last_name}</th>
@@ -41,7 +43,7 @@ export default class AdminUsers_Controller extends AdminBaseController
                     <th class="user__field user__role">${admin.users[i].account_type}</th>
                     <th class="user__field user__actions">
                         <div id="profile-delete" class="action actions__profile" onclick="window.open('${url}');">Profiel</div>
-                        <div id="profile-delete" class="action actions__delete">
+                        <div id="profile-delete" class="action actions__delete" onclick="user.destroy()">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                 <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
                             </svg>
