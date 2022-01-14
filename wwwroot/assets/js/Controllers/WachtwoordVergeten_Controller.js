6b10a2ca85c3f7f4c1d0827ccfac8c26850b2cee
Joli-Coeur Weibolt <joli-coeur.weibolt@hva.nl>
2022-01-14 07:16:08 +0100

    Changed wachtwoord_vergeten and made some minor changes on the login page and Register_Controller.js

--- wwwroot/assets/js/Controllers/WachtwoordVergeten_Controller.js
+++ wwwroot/assets/js/Controllers/WachtwoordVergeten_Controller.js
@@ -10,27 +10,39 @@ import view from "../Classes/View.js";
 export default class Wachtwoord_vergeten extends Controller
 {
     index() {
-            console.log("test")
-
             document.querySelector(".zoeken").addEventListener("click", event => {
-                FYSCloud.API.queryDatabase(
-                    "SELECT email FROM users"
-                ).then(function(data) {
-                    console.log(data)
-                    let invoer = document.getElementById("email").value;
-                    console.log(data.length)
-                    for (let i = 0; i <data.length ; i++) {
-                        let {email} = data[i];
-                        if(invoer === email){
-                            console.log("We hebben je account gevonden.")
-                        }
-                        console.log(email)
-                    }
+                let invoer = document.getElementById("email").value;
+                let ad = invoer.includes("@")
+                    FYSCloud.API.queryDatabase('SELECT email FROM users WHERE email = ?', [invoer])
+                        .then(function (data) {
+                            console.log(data)
+                                if(invoer === null || invoer === ""){
+                                    document.getElementById("error").innerHTML = "Je moet iets invoeren"
+                                    document.getElementById("error").style.color = "red"
+                                    console.log(data.length)
+                                }
+                                else if(invoer !== '@'){
+                                    document.getElementById("error").innerHTML = "Je moet een emailadres invoeren."
+                                    document.getElementById("error").style.color = "red"
+                                }
+                                if(data.length === 1){
+                                    document.getElementById("titel").innerHTML = "Er is een email verstuurd naar het ingevulde emailadres"
+                                    document.getElementById("titel").style.backgroundColor = "green"
+                                    document.getElementById("error").innerHTML = "Er is een account gevonden met dit emailadres!"
+                                    document.getElementById("error").style.color = " green"
+                                }
+                                if(ad === true && data.length === 0){
+                                    document.getElementById("error").innerHTML = "Er bestaat geen account met dit emailadres."
+                                    document.getElementById("error").style.color = "red"
+                                }
 
-                }).catch(function(reason) {
-                    console.log(reason);
-                });
+
+                        }).catch(function (reason) {
+                        console.log(reason);
+                    });
+
             })
+
     }
     render() {
         return new view('wachtwoord_vergeten.html',"CommonFlight | Password forgot").extends("blank.html");
