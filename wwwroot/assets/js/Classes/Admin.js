970b40cc961165a05c97d9ac5bd7bacf6d4f590e
diaq <diaqfort@gmail.com>
2022-01-14 07:45:27 +0100

    Moved destroy button from Profile, Updated function
    Now it deletes everything related to the users_id from every relevant table.
    
    There is probably an easier way with like ON CASCADE, but it at least works now!

--- wwwroot/assets/js/Classes/Admin.js
+++ wwwroot/assets/js/Classes/Admin.js
@@ -28,4 +28,32 @@ export default class Admin {
         }
     }
 
+    async destroy(userId) {
+        if (userId > 0) {
+            try {
+                await FYSCloud.API.queryDatabase("DELETE FROM user_countries WHERE user_id = ?", [userId]);
+                await FYSCloud.API.queryDatabase("DELETE FROM user_intressed WHERE user_id = ? OR intressed_id = ?", [userId, userId]);
+                await FYSCloud.API.queryDatabase("DELETE FROM user_matches WHERE requested_id = ? OR user_id = ?", [userId, userId]);
+                await FYSCloud.API.queryDatabase("DELETE FROM chat WHERE first_user = ? OR second_user = ?", [userId, userId]);
+                await FYSCloud.API.queryDatabase("DELETE FROM messages WHERE from_user_id = ? OR to_user_id = ?", [userId, userId]);
+                await FYSCloud.API.queryDatabase("DELETE FROM request WHERE user_id = ? OR to_user = ?", [userId, userId]);
+                let data = await FYSCloud.API.queryDatabase("DELETE FROM users WHERE id = ?", [userId]);
+                this.first_name = null;
+                this.last_name = null;
+                this.email = null;
+                this.account_type = null;
+                this.birthday = null;
+                this.profile = null;
+                this.gender = null;
+                this.verified_at = null;
+                this.country = null;
+                return data[0];
+
+            } catch (e) {
+                return {};
+            }
+
+        }
+    }
+
 }
